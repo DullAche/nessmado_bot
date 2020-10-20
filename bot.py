9e18d95f9f837963e2e78745859e2bded4199d91
DullAche <66999701+DullAche@users.noreply.github.com>
2020-10-20 17:04:56 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,7 +66,7 @@ async def on_message(message):
 
 # botの接続と起動
 # （tokenにはbotアカウントのアクセストークンを入れてください）
-client.run("NzI0MDU1NDgyMzMwNzc1NjEz.Xu6nNw.Qs3HALm7GXXJw0uQtHAW0IfXZs8")
+client.run("NzI0MDU1NDgyMzMwNzc1NjEz.Xu6nNw.ka3yyTs6Noo7GJUABNf5kHGdJKc")
 
 # -------------------ここでメイン終わり---------------------
 
